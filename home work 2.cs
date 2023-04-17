c0ee90a35e205bf02143d91ba6245bada1b6b230
Murad Gadzhiev <muradimgadzhiev@gmail.com>
2023-04-17 20:36:46 +0300

    done

---
+++ home work 2.cs
@@ -0,0 +1,38 @@
+// задача 10
+
+// System.Console.WriteLine("Видите число:");
+//     int number = int.Parse(Console.ReadLine());
+
+// int number2 = number / 10 ;
+// int number3 = number2 - number2 / 10 * 10;
+
+// System.Console.WriteLine(" " + number  + "->" + number3);
+
+
+
+// задача 13
+
+// System.Console.WriteLine("Видите число:");
+//     int number = int.Parse(Console.ReadLine());
+
+// if (number > 99) {
+//     int number2 = number % 10;
+//     System.Console.WriteLine($"{number}->{number2}");
+// }
+// else {
+//     System.Console.WriteLine($"{number}-> нет");
+// }
+
+
+
+// задача 15
+
+System.Console.WriteLine("Видите число:");
+    int number = int.Parse(Console.ReadLine());
+
+if (number < 6) {
+    System.Console.WriteLine(number+"-> нет");
+}
+else {
+    System.Console.WriteLine(number+"-> да");
+}
